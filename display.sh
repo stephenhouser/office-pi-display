91e6028685b6645ac25a6ed0be44d36efb112f4e
Stephen Houser <stephenhouser@gmail.com>
2018-01-19 12:49:56 -0500

    tweak shell version, node not working on pi

--- display.sh
+++ display.sh
@@ -11,17 +11,17 @@ do
 	ip_addr=$(ifconfig wlan0 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 	now=$(date +%-I:%M:%S)
 	today=$(date "+%a, %b %-d")
-	temp_c=$(/opt/vc/bin/vcgencmd measure_temp|sed -En 's/temp=([0-9]*\.[0-9]*).*/\1/p')
-	temp_f=$(echo "$temp_c * 1.8 + 32" | bc)
+	temp_c=$(owread /26.103D15000000/temperature)
+	temp_f=$(printf "%.1f" $(echo "scale=1; $temp_c * 1.8 + 32" | bc))
 
 	convert -size 320x480 background.png \
         	-font Roboto-Regular -fill yellow -pointsize 64  -gravity north  -draw "text 0,0 '$now'" \
         	-font Roboto-Regular -fill yellow -pointsize 48  -gravity north  -draw "text 0,75 '$today'" \
         	-font Roboto-Bold    -fill white -pointsize 120 -gravity center -draw "text 0,125 '$temp_f'" \
         	-font Roboto-Regular -fill skyblue   -pointsize 24  -gravity south  -draw "text 0,0 '$ip_addr'" \
-        	display320x480.jpg
+        	display.png
 
-	fbi -T 2 -d /dev/fb1 -noverbose -a display320x480.jpg 2> /dev/null
+	fbi -T 2 -d /dev/fb1 -noverbose -a display.png 2> /dev/null
 
 	sleep 0.75
 	kill $(pgrep fbi)
